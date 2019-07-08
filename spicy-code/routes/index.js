7b77ad051ec1bbc34244d5d0d3fa40631c35b7e8
Miriloper <miriam.mad.ml@gmail.com>
2019-07-08 21:31:40 +0200

    BE first things

--- spicy-code/routes/index.js
+++ spicy-code/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get("/profile"), (req, res) => {
+  res.render("/auth/profile")
+}
+
+router.get("/ranking"), (req, res) => {
+  res.render("/auth/ranking")
+}
+
+router.get("/lobby"), (req, res) => {
+  res.render("/auth/lobby")
+}
+
+router.get("/about-us"), (req, res) => {
+  res.render("/auth/about-us")
+}
+
+router.get("/play"), (req, res) => {
+  res.render("/auth/play")
+}
+
 module.exports = router;
