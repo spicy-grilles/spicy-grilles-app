ee3d11c43a9ccf0ca1d83e530089153517ffbd3e
Gabriel B <gabelmo92@hotmail.com>
2019-07-11 10:52:15 +0200

    dd

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,25 +1,30 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const restaurantSchema = new Schema({
-  username: String,
-  url: String,
-  image: String,
-  imagePath: String,
-  spicyPoints: {type: Number, enum: [50, 100, 150, 200, 250]},
-  activeItem: {type: Boolean, default: false},
-  item: {type: String}, //populate, cambiar con la learning
-  
-  location: { type: { type: String }, coordinates: [Number] }
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const restaurantSchema = new Schema(
+  {
+    username: String,
+    url: String,
+    image: String,
+    imagePath: String,
+    spicyPoints: { type: Number, enum: [50, 100, 150, 200, 250] },
+    activeItem: { type: Boolean, default: false },
+    item: { type: String }, //populate, cambiar con la learning
+
+    location: { type: { type: String }, coordinates: [Number] }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-restaurantSchema.index({ location: '2dsphere' });
+restaurantSchema.index({ location: "2dsphere" });
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
 module.exports = Restaurant;
+
+
